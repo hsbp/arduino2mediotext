b9b85fa4caaa2f683e8942ba92c970f1c1faeac4
András Veres-Szentkirályi <vsza@vsza.hu>
2013-04-17 18:41:15 +0200

    indentation and spacing fixes

--- arduino2mediotext.ino
+++ arduino2mediotext.ino
@@ -8,23 +8,22 @@ byte fb[SIZEX/8][SIZEY];
 int screenRow;
  
 void stepRow() {
-
-  updateFb(SIZEY-screenRow-1);
+  updateFb(SIZEY - screenRow - 1);
   screenRow--;
-  if (screenRow<0) {
-	  screenRow=6;
+  if (screenRow < 0) {
+    screenRow = 6;
   }
 
   cli();
-  PORTB=7;
+  PORTB = 7;
   for (uint8_t rx = 0; rx < (SIZEX / 8); rx++) {
-    for (int8_t i=7; i>=0; i--)  {       // clock pin12 (0x10) HIGH + data pin13 a framebuffer es \
+    for (int8_t i = 7; i >= 0; i--)  {   // clock pin12 (0x10) HIGH + data pin13 a framebuffer es \
                                          // aktiv sor szerint beallitva (<<5), valamint blanking (0xf)
       PORTB  =  0x1f | ( !( fb[rx][screenRow] & (1<<i) ) <<5);
       PORTB &= ~0x10; //clock pin12 LOW
     }
   }
-  PORTB=(screenRow);                     //select corresponding row
+  PORTB = (screenRow);                     //select corresponding row
   sei();
 }
 
@@ -33,9 +32,9 @@ void setup() {
     pinMode(i, OUTPUT);
   }
   for (uint8_t rx = 0; rx < 12; rx++) {
-  fb[rx][3]=1;
+    fb[rx][3] = 1;
   }
-  PORTB=0xf;
+  PORTB = 0xf;
   Timer1.initialize(2800);
   Timer1.attachInterrupt(stepRow);
 }
