a0b86d2977ed1ea50010a6d871f752ad440175a0
András Veres-Szentkirályi <vsza@vsza.hu>
2013-04-26 18:08:54 +0200

    added test suite for remote.Remote

---
+++ test_remote.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python
+
+from StringIO import StringIO
+import unittest
+import remote
+
+
+class TestRemote(unittest.TestCase):
+    def setUp(self):
+        self.remote = remote.Remote()
+
+    def test_init(self):
+        self.assertEqual(
+                self.get_serial_buf().getvalue(),
+                '\xC7\x0C' + '\x00' *
+                    (remote.COLS / remote.BIT_PER_BYTE * remote.ROWS))
+        for x in xrange(remote.COLS):
+            for y in xrange(remote.ROWS):
+                self.assertFalse(self.remote.get_pixel((x, y)))
+
+    def test_empty_flush(self):
+        sb = self.get_serial_buf()
+        sb.truncate(0)
+        self.remote.flush_pixels()
+        self.assertEquals(sb.getvalue(), '')
+		
+    def test_full(self):
+        sb = self.get_serial_buf()
+        sb.truncate(0)
+        for x in xrange(remote.COLS):
+            for y in xrange(remote.ROWS):
+                self.remote.set_pixel((x, y), True)
+        for x in xrange(remote.COLS):
+            for y in xrange(remote.ROWS):
+                self.assertTrue(self.remote.get_pixel((x, y)))
+        self.remote.flush_pixels()
+        self.assertEqual(
+                sb.getvalue(),
+                '\xC7\x0C' + '\xFF' *
+                    (remote.COLS / remote.BIT_PER_BYTE * remote.ROWS))
+
+    def get_serial_buf(self):
+        return self.remote.port.dut2tc
+
+
+class MockSerial(object):
+    def __init__(self, port, speed, timeout):
+        self.dut2tc = StringIO()
+        self.tried_read = False
+
+    def write(self, data):
+        pp = self.dut2tc.tell()
+        self.dut2tc.write(data)
+        self.dut2tc.seek(pp)
+
+    def read(self):
+        if self.tried_read:
+            self.tried_read = False
+            return 'B'
+        else:
+            self.tried_read = True
+            return ''
+
+
+remote.Serial = MockSerial
+
+if __name__ == '__main__':
+    unittest.main()
