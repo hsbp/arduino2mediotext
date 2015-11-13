8c755d87eb26085fe4172b57ae9a1142a7197484
András Veres-Szentkirályi <vsza@vsza.hu>
2015-11-13 11:28:52 +0100

    added simple test for C++ Remote class

--- remote-cpp/remote_main.cpp
+++ remote-cpp/remote_main.cpp
@@ -1,25 +1,37 @@
 #include "Remote.h"
 #include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+
+#define BUFLEN 512
+#define HANDLE_BUF(D, X, E) \
+	write(to_remote[1], "K", 1);\
+	X;\
+	bytes = read(from_remote[0], buf, BUFLEN);\
+	printf("[%s] ", D);\
+	if (memcmp(buf, E, bytes) != 0) printf("NOK "); else printf("OK ");\
+	for (int i = 0; i < bytes; i++) {\
+		printf("%02hhx", buf[i]);\
+	}\
+	putchar('\n');
 
 int main(int argc, char **argv) {
-	Remote r(0, 1);
+	int to_remote[2], from_remote[2];
+	int bytes;
+	char buf[BUFLEN];
+	pipe(to_remote);
+	pipe(from_remote);
+	HANDLE_BUF("test_init", Remote r(to_remote[0], from_remote[1]),
+			"\xC7\x0C\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0"
+			"\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0"
+			"\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0")
 	r.setPixel(0, 0, true);
-	r.flushPixels();
+	HANDLE_BUF("test_first_block", r.flushPixels(), "\xC1\x01\x80")
 	r.setPixel(8, 0, true);
-	r.flushPixels();
+	HANDLE_BUF("test_second_block", r.flushPixels(), "\xC1\x12\x80");
 	r.setPixel(9, 0, true);
+	HANDLE_BUF("test_second_block", r.flushPixels(), "\xC1\x12\xc0");
 	r.flushPixels();
-	r.flushPixels();
-	/*printf("---flushpixel1---\n");
-	r.flushPixels();
-	printf("---setpixel---\n");
-	r.setPixel(0, 0, true);
-	printf("---flushpixel2---\n");
-	r.flushPixels();
-	printf("---setpixel---\n");
-	r.setPixel(8, 3, false);
-	r.setPixel(1, 1, false);
-	printf("---flushpixel3---\n");
-	r.flushPixels();*/
+	printf("--- done ---\n");
 	return 0;
 }
